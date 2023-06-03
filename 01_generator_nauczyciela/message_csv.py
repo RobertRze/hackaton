c6916970bad3632ea6a1b21cf714fce9a9534b93
RobertRze <robert_rzeszotek@sevenet.pl>
2023-06-03 14:59:54 +0200

    01_Zadanie 01 - generator wiadomosci

---
+++ 01_generator_nauczyciela/message_csv.py
@@ -0,0 +1,40 @@
+import csv
+
+def get_student_from_file():
+    try:
+        with open("students.csv", 'r', encoding='utf-8') as rfile:
+            csv_data = csv.reader(rfile, delimiter=',')
+            names = []
+            tasks = []
+            grades = []
+            for student in csv_data:
+                names.append(student[1] + ' ' + student[2])
+                tasks.append(student[3])
+                grades.append(student[4].replace('\n', ''))
+            return names, tasks, grades
+    except FileNotFoundError as ferr:
+        print("Pliku nie znaleziono:", ferr)
+
+
+def get_message():
+    try:
+        with open("message.txt", 'r', encoding='utf-8') as rfile:
+            return rfile.read()
+    except FileNotFoundError as ferr:
+        print("Pliku nie znaleziono:", ferr)
+
+
+def complete_data(names, tasks, grades):
+    message = get_message()
+    try:
+        for name, task, grade in zip(names, tasks, grades):
+            print(message.format(name, task, grade, int(grade) + 1))
+    except ValueError as verr:
+        print("Nieprawidłowa wartość", verr)
+
+def main():
+    name, task, grade = get_student_from_file()
+    complete_data(name, task, grade)
+
+if __name__ == '__main__':
+    main()
